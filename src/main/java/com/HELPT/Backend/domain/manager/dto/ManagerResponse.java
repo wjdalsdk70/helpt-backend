d0121179bab646262491e712cbd04bdd3466af53
wjdals Lee <kyungheeyj45@ajou.ac.kr>
2024-04-09 18:57:52 +0900

    feat : Manager Response DTO 추가

---
+++ src/main/java/com/HELPT/Backend/domain/manager/dto/ManagerResponse.java
@@ -0,0 +1,18 @@
+package com.HELPT.Backend.domain.manager.dto;
+
+import com.HELPT.Backend.domain.gym.entity.Gym;
+import com.HELPT.Backend.domain.manager.Manager;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ManagerResponse {
+
+    private Long gymId;
+
+    @Builder
+    public ManagerResponse(Manager manager) {
+        this.gymId = manager.getGymId();
+    }
+}
