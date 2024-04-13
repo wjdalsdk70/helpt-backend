9ae9cf3e2719bf8b562b595447e7250b918183d0
이정민 <kyungheeyj45@ajou.ac.kr>
2024-04-14 00:59:00 +0900

    test : 헬스장 사업자 정보 테스트 데이터 추가

--- src/main/java/com/HELPT/Backend/global/config/DataInitializer.java
+++ src/main/java/com/HELPT/Backend/global/config/DataInitializer.java
@@ -2,6 +2,7 @@ package com.HELPT.Backend.global.config;
 
 import com.HELPT.Backend.domain.admin.Admin;
 import com.HELPT.Backend.domain.admin.AdminRepository;
+import com.HELPT.Backend.domain.gym.entity.GymRegistration;
 import com.HELPT.Backend.domain.gym.repository.GymRepository;
 import com.HELPT.Backend.domain.gym.entity.Gym;
 import com.HELPT.Backend.domain.gym.entity.Status;
@@ -28,9 +29,20 @@ public class DataInitializer{
             Admin admin = Admin.builder().loginId("wjdalsdk70").password(encodedPassword).role(ADMIN).build();
             adminRepository.save(admin);
         }
+        GymRegistration gr = GymRegistration.builder()
+                .contactNumber("010-1234-5678")
+                .businessNumber("123-45-67890")
+                .businessType("개인")
+                .OwnerName("홍길동")
+                .businessFile("/path/to/file.pdf")
+                .build();
+
         Gym gym = Gym.builder().gymName("나이스짐")
-                .status(Status.Pending)
+                .address("경기도 수원시 팔달구 아주로 37 아록빌딩 7층 나이스짐")
+                .gymRegistration(gr)
                 .build();
         gymRepository.save(gym);
+
+
     }
 }
