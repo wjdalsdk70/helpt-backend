34d16db528a7c030e216737d539cc978d96eab42
이정민 <kyungheeyj45@ajou.ac.kr>
2024-04-15 00:58:04 +0900

    feat : Equipment CRUD 로직 구현

--- src/main/java/com/HELPT/Backend/domain/equipment/Equipment.java
+++ src/main/java/com/HELPT/Backend/domain/equipment/Equipment.java
@@ -1,28 +1,44 @@
 package com.HELPT.Backend.domain.equipment;
 
+import com.HELPT.Backend.domain.gym.entity.Status;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
+@Builder
 @Getter
-@Setter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Equipment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long equipmentId;
 
-    private Long gymId;
+    private String equipmentName;
 
     private int defaultCount;
 
     private int defaultSet;
 
-    private float defaultWeight;
+    private int defaultWeight;
 
-    private String equipmentName;
+    public void updateEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public void updateDefaultCount(int defaultCount) {
+        this.defaultCount = defaultCount;
+    }
+
+    public void updateDefaultSet(int defaultSet) {
+        this.defaultSet = defaultSet;
+    }
+
+    public void updateDefaultWeight(int defaultWeight) {
+        this.defaultWeight = defaultWeight;
+    }
 }
