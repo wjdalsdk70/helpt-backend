49fad605a3ed51f588f84c67f3ea4c980d3162fa
이정민 <kyungheeyj45@ajou.ac.kr>
2024-05-03 02:41:58 +0900

    feat : 공통 response 처리 config 추가

---
+++ src/main/java/com/HELPT/Backend/global/config/ResponseAdviceConfig.java
@@ -0,0 +1,36 @@
+package com.HELPT.Backend.global.config;
+
+import com.HELPT.Backend.global.common.ApiResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ResponseAdviceConfig implements ResponseBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+//        if(body instanceof String){
+//            return body;
+//        }else if(body instanceof ResponseDto){
+//            ((ResponseDto) body).setStatus(status);
+//            return body;
+//        }else{
+//            return new ResponseDto(status,body);
+//        }
+        if(body instanceof String){
+            return body;
+        }else{
+            int status = ((ServletServerHttpResponse)response).getServletResponse().getStatus();
+            return ApiResponse.successResponse(body);
+        }
+    }
+}
