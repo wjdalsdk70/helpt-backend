f51995b4613a6f5c848ee258f9febe4189ff6b83
이정민 <kyungheeyj45@ajou.ac.kr>
2024-05-12 20:50:45 +0900

    feat : 회원 자신의 정보 조회 및 회원 이름으로 검색 기능 추가

--- src/main/java/com/HELPT/Backend/domain/member/MemberRepositoryCustom.java
+++ src/main/java/com/HELPT/Backend/domain/member/MemberRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.HELPT.Backend.domain.member;
 
+import com.HELPT.Backend.domain.manager.dto.MemberJoinResponse;
 import com.HELPT.Backend.domain.membership.Membership;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
     public Membership attendance(Long userid);
+    public List<MemberJoinResponse> memberList(Long gymId, String name);
 
 }
--- src/main/java/com/HELPT/Backend/domain/member/MemberRepositoryImpl.java
+++ src/main/java/com/HELPT/Backend/domain/member/MemberRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.HELPT.Backend.domain.member;
 
+import com.HELPT.Backend.domain.manager.dto.MemberJoinResponse;
 import com.HELPT.Backend.domain.membership.Membership;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.HELPT.Backend.domain.member.QMember.member;
 import static com.HELPT.Backend.domain.membership.QMembership.membership;
 
 @Repository
@@ -33,5 +37,25 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
         return queryFactory.selectFrom(membership)
                 .where(membership.userId.eq(userid))
                 .fetchOne();
+    }
+
+    @Override
+    public List<MemberJoinResponse> memberList(Long gymId, String name)
+    {
+        BooleanExpression namePredicate = member.userName.contains(name);
+
+        List<Tuple> results = queryFactory
+                .select(member.userId, member.userName, membership.startDate, membership.endDate)
+                .from(member)
+                .leftJoin(membership).on(member.userId.eq(membership.userId))
+                .where(member.gymId.eq(gymId).and(namePredicate))
+                .fetch();
+
+        return results.stream().map(tuple -> MemberJoinResponse.builder()
+                .userId(tuple.get(member.userId))
+                .userName(tuple.get(member.userName))
+                .startDate(tuple.get(membership.startDate))
+                .endDate(tuple.get(membership.endDate))
+                .build()).toList();
     }
 }
