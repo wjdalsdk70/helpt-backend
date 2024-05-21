cad87bf121607c6be1f74046671d49abfe3cfe2e
이정민 <kyungheeyj45@ajou.ac.kr>
2024-05-21 22:42:43 +0900

    fix : 회원권 null 에러 로직 처리

--- src/main/java/com/HELPT/Backend/domain/membership/MembershipService.java
+++ src/main/java/com/HELPT/Backend/domain/membership/MembershipService.java
@@ -1,11 +1,14 @@
 package com.HELPT.Backend.domain.membership;
 import com.HELPT.Backend.domain.membership.dto.MembershipResponse;
 import com.HELPT.Backend.domain.product.Product;
+import com.HELPT.Backend.global.error.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+
+import static com.HELPT.Backend.global.error.ErrorCode.NOT_EXIST_DATA;
 
 
 @Service
@@ -17,7 +20,7 @@ public class MembershipService {
 
     public MembershipResponse findMembership(Long userid)
     {
-        Membership findMembership = membershipRepository.findByUserId(userid).orElseThrow(() -> new RuntimeException("Membership not found"));
+        Membership findMembership = membershipRepository.findByUserId(userid).orElseThrow(() -> new CustomException(NOT_EXIST_DATA));
         return new MembershipResponse(findMembership);
     }
 
@@ -45,7 +48,7 @@ public class MembershipService {
 
     public MembershipResponse extensionMembership(Long membershipId, LocalDate endDate)
     {
-        Membership findMembership = membershipRepository.findById(membershipId).orElseThrow(() -> new RuntimeException("Membership not found"));
+        Membership findMembership = membershipRepository.findById(membershipId).orElseThrow(() -> new CustomException(NOT_EXIST_DATA));
         findMembership.setEndDate(endDate);
         return new MembershipResponse(findMembership);
     }
