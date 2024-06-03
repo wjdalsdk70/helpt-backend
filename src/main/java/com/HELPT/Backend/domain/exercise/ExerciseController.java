fc24e3aac05d5d2ee14ade43fda823f47dac28c4
dlsqja <dlsqja3436@gmail.com>
2024-06-03 14:52:41 +0900

    feat : test file

--- src/main/java/com/HELPT/Backend/domain/exercise/ExerciseController.java
+++ src/main/java/com/HELPT/Backend/domain/exercise/ExerciseController.java
@@ -37,4 +37,9 @@ public class ExerciseController {
     public ResponseEntity<ExerciseResponseDto> exerciseDetails(@PathVariable Long gymEquipmentId) {
         return ResponseEntity.ok(exerciseService.findExercise(gymEquipmentId));
     }
+
+    @GetMapping("/{exerciseId}")
+    public ResponseEntity<ExerciseResponseDto> exerciseDetailss(@PathVariable Long gymEquipmentId) {
+        return ResponseEntity.ok(exerciseService.findExercise(gymEquipmentId));
+    }
 }
