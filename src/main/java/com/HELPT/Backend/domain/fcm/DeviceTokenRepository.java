80c15cbf53848911d7ff8be3bd81ad1d4f4fda77
이정민 <kyungheeyj45@ajou.ac.kr>
2024-06-10 03:50:24 +0900

    fix : 중복된 토큰 저장 방지

--- src/main/java/com/HELPT/Backend/domain/fcm/DeviceTokenRepository.java
+++ src/main/java/com/HELPT/Backend/domain/fcm/DeviceTokenRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
 
+    List<DeviceToken> findAllByUserId(Long userId);
+
     @Query("SELECT dt.deviceToken FROM DeviceToken dt JOIN Member m ON dt.userId = m.userId WHERE m.gymId = :gymId")
     List<String> findDeviceTokensByGymId(@Param("gymId") Long gymId);
 }
