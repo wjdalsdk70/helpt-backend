2102b1aa632d7431c7278f3dfe88d82971e64928
dlsqja <dlsqja3436@gmail.com>
2024-06-09 23:50:15 +0900

    feat : 시간대 변경

--- src/main/java/com/HELPT/Backend/BackendApplication.java
+++ src/main/java/com/HELPT/Backend/BackendApplication.java
@@ -1,13 +1,23 @@
 package com.HELPT.Backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
